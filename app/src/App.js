8514d9d3ea7e10284822ff655e0cdff3876f3c22
Yimerwolley27 <80656574+Yimerwolley27@users.noreply.github.com>
2022-12-13 16:42:54 -0500

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p> HELLO WORLD</p>
-        <p> worksssss</p>
+        <p> works</p>
         <a
           className="App-link"
           href="https://reactjs.org"
