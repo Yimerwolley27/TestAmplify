5f16f801283579e0155a3547b312359004b0b3f7
Yeabsera Yimer Wolle <yimerwolley@vcu.edu>
2022-12-15 11:11:11 -0500

    rename my-app to app

---
+++ app/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Inputbox from './components/Inputbox.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+        <img className="logo" src ="https://www.costargroup.com/images/librariesprovider3/costar-group/costar_group-logo.png?sfvrsn=2" alt="React Image" />
+        </h1>
+        <button> Filtered Search Page </button>
+      </header>
+      <body className="App-body">
+        <div>
+          <Inputbox></Inputbox>
+        </div>
+      </body>
+      <footer className="App-footer">
+        FOOTER
+      </footer>
+    </div>
+  );
+}
+
+export default App;
