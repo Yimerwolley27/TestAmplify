496fadbd770fa3e50586c6ca38150df1f4c8080e
Yeabsera Yimer Wolle <yimerwolley@vcu.edu>
2023-01-31 19:45:29 -0500

    updating branch

--- src/components/Inputbox.js
+++ src/components/Inputbox.js
@@ -8,35 +8,41 @@ const ButtonClick = () => {
     setMessage(event.target.value);
   };
 
-  const fetchData = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setUsers(data)
-      })
-  }
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://lnfppvwcta.execute-api.us-east-1.amazonaws.com/v1", {
+        method: "POST",
+        body: JSON.stringify({
+          listingId: listingId,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessage("ListingId correct");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+ 
   return (
     <div>
-      <input
-        type="text"
-        id="message"
-        name="message"
-        onChange={handleChange}
-        value={message}
-      />
-
-      <button onClick={fetchData}>Update</button>
-      {users.length > 0 && (
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      )}
+      <form onSubmit={{handleSubmit}}>
+        <input
+          type="text"
+          id="message"
+          name="message"
+          onChange={handleChange}
+          value={message}
+        />
+      </form>
+      <button type ="Submit"> Create</button>
+      <div className="message">{message ? <p>{message}</p> : null}</div>
     </div>
   );
-}
 
-export default ButtonClick
+  }
+  export default ButtonClick;
