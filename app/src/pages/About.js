9ef67a6b2f34a3d3e934d5f040cdbeacb5fb3e6f
Yeabsera Yimer Wolle <yimerwolley@vcu.edu>
2023-03-08 12:43:54 -0500

    testing amplify

--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -11,7 +11,7 @@ export default function About() {
             </div>
             <div className="about-body">
                 <p2>
-                    CoStar Group, Inc. (NASDAQ: CSGP) is the leading provider of commercial real estate information, analytics and online marketplaces. Founded in 1987, CoStar conducts expansive, ongoing research to produce and maintain the largest and most comprehensive database of commercial real estate information.
+                    Test for Amplify----CoStar Group, Inc. (NASDAQ: CSGP) is the leading provider of commercial real estate information, analytics and online marketplaces. Founded in 1987, CoStar conducts expansive, ongoing research to produce and maintain the largest and most comprehensive database of commercial real estate information.
                 </p2>
             <p>
             CoStar Group's core objective is to provide a rich and comprehensive view of listings across markets to home shoppers. Residential listings miss important information and are not standardized across markets. However, key data points are presented as textual descriptions by listing agents. Currently, a large amount of research resources is devoted to building standardization of data points across markets. an automated system to extract key information from listings descriptions that map to our residential data schema so that we can provide a comprehensive view to home shoppers about a home. Residential listings are missing a lot of important information. However, many of the key data points are present in the listing description. We want to build an automated key information extraction system to extract key information from listings descriptions to enrich the residential data.
