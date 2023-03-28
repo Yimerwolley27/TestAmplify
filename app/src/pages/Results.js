1e4b54aee1f05e9504065ab9abdc0611b57ac6a3
KLL <kllyyy68@gmail.com>
2023-03-28 13:54:06 -0400

    updated styling

--- app/src/pages/Results.js
+++ app/src/pages/Results.js
@@ -2,12 +2,12 @@ import SearchResult from "../components/SearchResult";
 
 export default function Results() {
     return (
-      <body classNameName="App-body">
+      <div className="pt-5">
         <div className="card">
-        <div className="card-body">
-          <SearchResult></SearchResult>
+          <div className="card-body">
+            <SearchResult></SearchResult>
+          </div>
         </div>
       </div>
-      </body>
     )
 }
