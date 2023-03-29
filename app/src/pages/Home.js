4f174caed137c893466bdbc05dfb20859e748253
Yeabsera Yimer Wolle <yimerwolley@vcu.edu>
2023-03-29 15:22:26 -0400

    dummy text to homepage

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -9,11 +9,9 @@ export default function Home() {
     </div>
     <div className="about-body">
         <p>
-            Enriching Residential Data by Extracting Key Information from Listing Description
-        </p>
+        CoStar Group's core objective is to provide a rich and comprehensive view of listings across markets to home shoppers. Residential listings miss important information and are not standardized across markets. However, key data points are presented as textual descriptions by listing agents. Currently, a large amount of research resources is devoted to building standardization of data points across markets. an automated system to extract key information from listings descriptions that map to our residential data schema so that we can provide a comprehensive view to home shoppers about a home. Residential listings are missing a lot of important information. However, many of the key data points are present in the listing description. We want to build an automated key information extraction system to extract key information from listings descriptions to enrich the residential data. </p>
     </div>
     <footer>
-      something here
     </footer>
 </div>
   )
