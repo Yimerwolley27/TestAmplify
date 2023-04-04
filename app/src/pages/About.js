dd2e5c9f451b0189e8d1fc318c3943c50382ff89
Gabrielle <corneliusga@mymail.vcu.edu>
2023-04-04 12:43:34 -0400

    added photos

--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -4,23 +4,23 @@ export default function About() {
         <div>
         <table className = "table">
         <tr>
-          <td>John Leonard<br /><img src="about_people.jpg" /><br />Faculty Advisor</td>
+          <td>John Leonard<br /><img src="leonard.jpg" /><br />Faculty Advisor</td>
           <td>Description<br /> <br />Describe the role this team member played</td>
         </tr>
         <tr>
-          <td>Khajamoinuddin Syed<br /><img src="about_people.jpg" /><br />Mentor</td>
+          <td>Khajamoinuddin Syed<br /><img src="syed.jpg" /><br />Mentor</td>
           <td>Description<br /> <br />Senior Machine Learning Engineer with CoStar Group. Received his PhD in Computer Science from VCU in May 2019 and has served as the main point of contact for the VCU team. Seen as the Project Manager, he would communicate what the project would need and would offer guidance whenever the team encountered roadblocks.</td>
         </tr>
         <tr>
-          <td>Gabrielle Cornelius<br /><img src="about_people.jpg" /><br />Student Team Member</td>
+          <td>Gabrielle Cornelius<br /><img src="gabbi.jpg" /><br />Student Team Member</td>
           <td>Description<br /> <br />Gabrielle will be receiving her B.S. in Computer Science and will be working with CoStar Group as an intern during Summer 2023. Originally from Stafford, VA, she hopes to work as a Data Engineer once she graduates in December 2023 from VCU.</td>
         </tr>
         <tr>
-          <td>Kim Long Ly<br /><img src="about_people.jpg" /><br />Student Team Member</td>
+          <td>Kim Long Ly<br /><img src="kim.jpg" /><br />Student Team Member</td>
           <td>Description<br /> <br />A senior studying Computer Science at VCU. From Virginia Beach, VA, he hopes to start working as a Software/Cloud Computing Engineer once he graduates in December 2023 with his bachelor’s.</td>
         </tr>
         <tr>
-          <td>Jeffrey Stanley<br /><img src="about_people.jpg" /><br />Student Team Member</td>
+          <td>Jeffrey Stanley<br /><img src="jeff.jpg" /><br />Student Team Member</td>
           <td>Description<br /> <br />A senior from VCU graduating in May/2023 with a B.S. in Computer Science. Originally from Buckingham, VA, he plans to one day go into research but will be starting out in Software Engineering in order to improve his skill sets before pursuing further education.</td>
         </tr>
         <tr>
