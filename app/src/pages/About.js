ac3372076984b7ca36f9fd9886e2f9f9e6864e26
Gabrielle <corneliusga@mymail.vcu.edu>
2023-04-04 12:17:48 -0400

    Testing about page description for gabby

--- app/src/pages/About.js
+++ app/src/pages/About.js
@@ -13,7 +13,7 @@ export default function About() {
         </tr>
         <tr>
           <td>Gabrielle Cornelius<br /><img src="about_people.jpg" /><br />Student Team Member</td>
-          <td>Description<br /> <br />Describe the role this team member played</td>
+          <td>Description<br /> <br />Gabrielle will be receiving her B.S. in Computer Science and will be working with CoStar Group as an intern during Summer 2023. Originally from Stafford, VA, she hopes to work as a Data Engineer once she graduates in December 2023 from VCU.</td>
         </tr>
         <tr>
           <td>Kim Long Ly<br /><img src="about_people.jpg" /><br />Student Team Member</td>
